9029ca34eaf8a667ff7738ed10f9063f5737fbfe
ole <ole@67ed7778-7388-44ab-90cf-0a291f65f57c>
2000-03-11 00:49:09 +0000

    Fixes invisble scrollbar
    git-svn-id: svn://svn.code.sf.net/p/gphoto/code/trunk@473 67ed7778-7388-44ab-90cf-0a291f65f57c

--- gphoto/src/main.c
+++ gphoto/src/main.c
@@ -153,7 +153,7 @@ int main (int argc, char *argv[]) {
 	gtk_window_set_title (GTK_WINDOW(mainWin), title);
 	gtk_signal_connect (GTK_OBJECT(mainWin), "delete_event",
 			    GTK_SIGNAL_FUNC(delete_event), NULL);
-	gtk_widget_set_usize(mainWin, 710, 480);
+	gtk_widget_set_usize(mainWin, 730, 480);
 	gtk_widget_realize(mainWin);
 
 	/* set up the menu --------------------------------------- */

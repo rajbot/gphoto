9a462818fa5754e8dc68abc1489b1aac2a79c600
scottf <scottf@67ed7778-7388-44ab-90cf-0a291f65f57c>
2000-06-08 21:08:30 +0000

    *** empty log message ***
    git-svn-id: svn://svn.code.sf.net/p/gphoto/code/trunk@538 67ed7778-7388-44ab-90cf-0a291f65f57c

--- libgpio/gpio-parallel.h
+++ libgpio/gpio-parallel.h
@@ -1,4 +1,39 @@
-/* Parallel port specific settings */
+/* PARALLEL port prefix for enumeration */
+
+/* Linux */
+#ifdef __linux__
+#define GPIO_PARALLEL_PREFIX "/dev/lp%i"
+#endif
+
+/* BSD */
+#if defined(__FreeBSD__) || defined(__NetBSD__)
+#define GPIO_PARALLEL_PREFIX NULL
+#endif
+
+/* Solaris */
+#ifdef sun
+#define GPIO_PARALLEL_PREFIX NULL
+#endif
+
+/* BeOS */
+#ifdef beos
+/* ????????????? */
+#define GPIO_PARALLEL_PREFIX NULL
+#endif
+
+/* Windows */
+#ifdef WIN
+#define GPIO_PARALLEL_PREFIX "LPT%i:"
+#endif
+
+/* Others? */
+
+/* Default */
+#ifndef GPIO_PARALLEL_PREFIX
+#define GPIO_PARALLEL_PREFIX NULL
+#endif
+
+/* PARALLEL port specific settings */
 typedef struct {
         char port[128];
 } gpio_parallel_settings;
--- libgpio/gpio-serial.h
+++ libgpio/gpio-serial.h
@@ -1,3 +1,39 @@
+/* Serial port prefix for enumeration */
+/* %i for numbers, %c for letters */
+
+/* Linux */
+#ifdef linux
+#define GPIO_SERIAL_PREFIX "/dev/ttyS%i"
+#endif
+
+/* BSD */
+#if defined(__FreeBSD__) || defined(__NetBSD__)
+#define GPIO_SERIAL_PREFIX "/dev/tty0%i"
+#endif
+
+/* Solaris */
+#ifdef sun
+#define GPIO_SERIAL_PREFIX "/dev/ttya%i"
+#endif
+
+/* BeOS */
+#ifdef beos
+/* ????????????? */
+#define GPIO_SERIAL_PREFIX NULL
+#endif
+
+/* Windows */
+#ifdef WIN
+#define GPIO_SERIAL_PREFIX "COM%i:"
+#endif
+
+/* Others? */
+
+/* Default */
+#ifndef GPIO_SERIAL_PREFIX
+#define GPIO_SERIAL_PREFIX NULL
+#endif
+
 /* Serial port specific settings */
 typedef struct {
         char port[128];

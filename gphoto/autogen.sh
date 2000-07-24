ea3bd395995b3b7dae559264d300e0c237d3ca02
ole <ole@67ed7778-7388-44ab-90cf-0a291f65f57c>
2000-07-24 11:27:32 +0000

    *** empty log message ***
    git-svn-id: svn://svn.code.sf.net/p/gphoto/code/trunk@750 67ed7778-7388-44ab-90cf-0a291f65f57c

--- gphoto/autogen.sh
+++ gphoto/autogen.sh
@@ -1,17 +1,4 @@
 #!/bin/sh
-# Run this to generate all the initial makefiles, etc.
+echo "Deprecated, check out the gphoto2 module."
+exit 0
 
-srcdir=`dirname $0`
-test -z "$srcdir" && srcdir=.
-
-PKG_NAME="gPhoto"
-
-(test -f $srcdir/configure.in \
-  && test -d $srcdir/src \
-  && test -f $srcdir/src/gphoto.h) || {
-    echo -n "**Error**: Directory "\`$srcdir\'" does not look like the"
-    echo " top-level $PKG_NAME directory"
-    exit 1
-}
-
-. $srcdir/macros/autogen.sh

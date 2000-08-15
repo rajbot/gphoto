32a6bf4446d33a46328d37a28cd322b91f5a0319
scottf <scottf@67ed7778-7388-44ab-90cf-0a291f65f57c>
2000-08-15 18:41:25 +0000

    added dynamic loading defines
    git-svn-id: svn://svn.code.sf.net/p/gphoto/code/trunk@883 67ed7778-7388-44ab-90cf-0a291f65f57c

--- libgpio/libgpio/library.h
+++ libgpio/libgpio/library.h
@@ -1,3 +1,16 @@
+#ifdef WIN32
+#define DLOPEN(_handle, _filename)
+#define DLSYM(_pointer, _handle, _funcname)
+#define DLCLOSE(_handle)
+#define DLERROR()
+#else
+#include <dlfcn.h>
+#define DLOPEN(_handle, _filename)		(_handle = dlopen(_filename, RTLD_LAZY))
+#define DLSYM(_pointer, _handle, _funcname)	(_pointer = dlsym(_handle, _funcname))
+#define DLCLOSE(_handle)			(dlclose(_handle))
+#define DLERROR()				(dlerror())
+#endif
+
 int gpio_library_list(gpio_device_info *list, int *count);
 int gpio_library_load(gpio_device *device, gpio_device_type type);
 int gpio_library_close(gpio_device *device);

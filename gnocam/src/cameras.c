be930524575d8e1b8c23e13fffb6cf524c74c34c
lutz <lutz@67ed7778-7388-44ab-90cf-0a291f65f57c>
2000-10-04 15:17:21 +0000

    Bug fix. Forgot a camera-> in front of frontend_data.
    git-svn-id: svn://svn.code.sf.net/p/gphoto/code/trunk@1106 67ed7778-7388-44ab-90cf-0a291f65f57c

--- gnocam/src/cameras.c
+++ gnocam/src/cameras.c
@@ -48,7 +48,7 @@ camera_tree_item_remove (GtkTreeItem* item)
 	g_assert ((path = gtk_object_get_data (GTK_OBJECT (item), "path")) != NULL);
 	g_assert ((tree = GTK_TREE (GTK_WIDGET (item)->parent)) != NULL);
 	g_assert ((camera = gtk_object_get_data (GTK_OBJECT (item), "camera")) != NULL);
-	g_assert ((frontend_data = (frontend_data_t*) frontend_data) != NULL);
+	g_assert ((frontend_data = (frontend_data_t*) camera->frontend_data) != NULL);
 
 	/* Root folder needs special care. */
 	root = ((!(filename = gtk_object_get_data (GTK_OBJECT (item), "filename"))) && (strcmp ("/", path) == 0));

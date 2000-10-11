982704e5178bc3ee83159cfb702d4a26de3e9e45
lutz <lutz@67ed7778-7388-44ab-90cf-0a291f65f57c>
2000-10-11 21:43:21 +0000

    gp_file_ref / gp_file_unref are now in libgphoto2.
    git-svn-id: svn://svn.code.sf.net/p/gphoto/code/trunk@1187 67ed7778-7388-44ab-90cf-0a291f65f57c

--- gnocam/src/gphoto-extensions.h
+++ gnocam/src/gphoto-extensions.h
@@ -3,11 +3,7 @@ gboolean description_extract (gchar* description, guint* id, gchar** name, gchar
 
 Camera* gp_camera_new_by_description (gchar* description);
 
-CameraWidget* gp_widget_clone (CameraWidget* widget);
-
 void gp_camera_ref 	(Camera* camera);
 void gp_camera_unref 	(Camera* camera);
 
-void gp_file_ref 	(CameraFile* file);
-void gp_file_unref 	(CameraFile* file);
 

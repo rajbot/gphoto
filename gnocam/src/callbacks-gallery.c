3438dfb3b83ee27b12c681f7b730de515b09ebfc
lutz <lutz@67ed7778-7388-44ab-90cf-0a291f65f57c>
2000-10-24 21:15:42 +0000

    Drop thumbnails onto the gallery works now.
    git-svn-id: svn://svn.code.sf.net/p/gphoto/code/trunk@1257 67ed7778-7388-44ab-90cf-0a291f65f57c

--- gnocam/src/callbacks-gallery.c
+++ gnocam/src/callbacks-gallery.c
@@ -3,6 +3,9 @@
 #include <glade/glade.h>
 #include <gconf/gconf-client.h>
 #include <gphoto2.h>
+#include <HTMLparser.h>
+#include <HTMLtree.h>
+#include <parser.h>
 #ifdef GNOCAM_USES_GTKHTML
 #  include <bonobo.h>
 #  include <bonobo/bonobo-stream-memory.h>
@@ -34,38 +37,54 @@ void on_editor_drag_data_received (GtkWidget* widget, GdkDragContext* context, g
         CORBA_Object            interface;
         CORBA_Environment       ev;
         BonoboObjectClient*     client;
-	BonoboStream*		stream;
-	Bonobo_Stream		bonobo_stream;
-	gchar*			text;
+	xmlDocPtr		doc;
+	xmlNodePtr		node;
+	xmlNodePtr		node_new;
+	GList*			filenames;
+	gint			i;
 
+	/***********************************************************************/
+	/* This is an ugly hack. I have _no_ idea how to edit a bonobo_stream. */
+	/* And, it's ugly code. I wrote it only to show you that it works.     */
+	/* Rewrite it. Do it. Now!                                             */
+	/***********************************************************************/
+
+	/* Preliminary stuff. */
 	CORBA_exception_init (&ev);
 	g_assert ((client = bonobo_widget_get_server (BONOBO_WIDGET (widget))));
-	g_assert ((interface =  bonobo_object_client_query_interface (client, "IDL:Bonobo/PersistStream:1.0", NULL)));
 
-	/* Get the current content. */
-//FIXME:
-	g_assert ((stream = bonobo_stream_mem_create ("Sorry, not yet implemented.", strlen ("Sorry, not yet implemented."), FALSE, TRUE)));
-	bonobo_stream = bonobo_object_corba_objref (BONOBO_OBJECT (stream));
-//	bonobo_stream_client_read_string (bonobo_stream, &text, &ev);
-//	g_assert (ev._major == CORBA_NO_EXCEPTION);
-//	printf ("TEXT:%s\n", text);
-//	bonobo_stream_client_write_string (bonobo_stream, "Sorry, not yet implemented!", TRUE, &ev);
-//	g_assert (ev._major == CORBA_NO_EXCEPTION);
-//	bonobo_stream_client_read_string (bonobo_stream, &text, &ev);
-//	g_assert (ev._major == CORBA_NO_EXCEPTION);
-//	printf ("TEXT:%s\n", text);
-
-	/* Add the picture. */
-//FIXME: Implement that.
-
-	/* Set the new content. */
-//FIXME: Implement that.
-	Bonobo_PersistStream_load (interface, bonobo_stream, "text/html", &ev);
-	if (ev._major != CORBA_NO_EXCEPTION) dialog_information (_("Could not load the new gallery!"));
-	if (ev._major != CORBA_SYSTEM_EXCEPTION) CORBA_Object_release (interface, &ev);
+	/* Get the current content and save it to "/tmp/editor". */
+	g_assert ((interface =  bonobo_object_client_query_interface (client, "IDL:Bonobo/PersistFile:1.0", NULL)));
+	Bonobo_PersistFile_save (interface, "/tmp/editor", &ev);
+	g_assert (ev._major == CORBA_NO_EXCEPTION);
 	Bonobo_Unknown_unref (interface, &ev);
+	CORBA_Object_release (interface, &ev);
+
+	/* Modify the gallery. */
+	filenames = gnome_uri_list_extract_filenames (selection_data->data);
+	g_assert ((doc = htmlParseFile ("/tmp/editor", NULL)));
+	printf ("\nBefore modification:\n");
+	htmlDocDump (NULL, doc);
+	node = xmlDocGetRootElement (doc);
+	node = xmlGetLastChild (node);
+	for (i = 0; i < g_list_length (filenames); i++) {
+		node_new = xmlNewChild (node, NULL, "img", NULL);
+		xmlSetProp (node_new, "src", g_strdup_printf ("file:%s", (gchar*) g_list_nth_data (filenames, i)));
+	}
+	printf ("\nAfter modification:\n");
+	htmlDocDump (NULL, doc);
+	htmlSaveFile ("/tmp/editor", doc);
+	gnome_uri_list_free_strings (filenames);
+
+	/* Load "/tmp/editor" into the html-editor. */
+	g_assert ((interface =  bonobo_object_client_query_interface (client, "IDL:Bonobo/PersistFile:1.0", NULL)));
+	Bonobo_PersistFile_load (interface, "/tmp/editor", &ev);
+	g_assert (ev._major == CORBA_NO_EXCEPTION);
+	Bonobo_Unknown_unref (interface, &ev);
+	CORBA_Object_release (interface, &ev);
+
+	/* Clean up. */
 	CORBA_exception_free (&ev);
-	bonobo_object_unref (BONOBO_OBJECT (stream));
 }
 
 void
@@ -100,6 +119,7 @@ on_app_gallery_save_as_activate (GtkMenuItem* menuitem, gpointer user_data)
 void
 on_app_gallery_clear_activate (GtkMenuItem* menuitem, gpointer user_data)
 {
+	dialog_information (_("Not yet implemented!"));
 }
 
 #endif

f6de7ea881312ac5a5e17d4499bbbb33de77578d
lutz <lutz@67ed7778-7388-44ab-90cf-0a291f65f57c>
2000-10-30 13:26:08 +0000

    No longer needed.
    git-svn-id: svn://svn.code.sf.net/p/gphoto/code/trunk@1278 67ed7778-7388-44ab-90cf-0a291f65f57c

--- gnocam/src/callbacks.h
+++
@@ -1,6 +0,0 @@
-
-/* Prototypes */
-
-void on_preferences_activate            (GtkMenuItem* menuitem, gpointer user_data);
-void on_about_activate                  (GtkMenuItem* menuitem, gpointer user_data);
-

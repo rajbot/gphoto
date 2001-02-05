e9642e53a030e9973792dde84e766a64b94f83d3
lutz <lutz@67ed7778-7388-44ab-90cf-0a291f65f57c>
2001-02-05 07:04:54 +0000

    Support Bonobo/Storage, too.
    git-svn-id: svn://svn.code.sf.net/p/gphoto/code/trunk@1558 67ed7778-7388-44ab-90cf-0a291f65f57c

--- gnocam/bonobo-moniker/bonobo-moniker-camera.c
+++ gnocam/bonobo-moniker/bonobo-moniker-camera.c
@@ -16,15 +16,28 @@ camera_resolve (BonoboMoniker *moniker, const Bonobo_ResolveOptions *options, co
 {
 	g_print ("Resolving as '%s'\n", requested_interface);
 
+	/* Stream? */
 	if (strcmp (requested_interface, "IDL:Bonobo/Stream:1.0") == 0) {
 		BonoboStream *stream;
 
-		stream = bonobo_stream_open_full ("camera", bonobo_moniker_get_name (moniker), Bonobo_Storage_READ, 0644, ev);
-
+		stream = bonobo_stream_open_full ("camera", bonobo_moniker_get_name (moniker), Bonobo_Storage_READ | Bonobo_Storage_WRITE, 0644, ev);
+		if (BONOBO_EX (ev)) stream = bonobo_stream_open_full ("camera", bonobo_moniker_get_name (moniker), Bonobo_Storage_READ, 0644, ev);
 		if (BONOBO_EX (ev)) return CORBA_OBJECT_NIL;
 		g_return_val_if_fail (stream != NULL, CORBA_OBJECT_NIL);
 
 		return CORBA_Object_duplicate (BONOBO_OBJREF (stream), ev);
+	}
+
+	/* Storage? */
+	if (strcmp (requested_interface, "IDL:Bonobo/Storage:1.0") == 0) {
+		BonoboStorage *storage;
+
+		storage = bonobo_storage_open_full ("camera", bonobo_moniker_get_name (moniker), Bonobo_Storage_READ | Bonobo_Storage_WRITE, 0644, ev);
+		if (BONOBO_EX (ev)) storage = bonobo_storage_open_full ("camera", bonobo_moniker_get_name (moniker), Bonobo_Storage_READ, 0644, ev);
+		if (BONOBO_EX (ev)) return CORBA_OBJECT_NIL;
+		g_return_val_if_fail (storage != NULL, CORBA_OBJECT_NIL);
+
+		return CORBA_Object_duplicate (BONOBO_OBJREF (storage), ev);
 	}
 
 	return bonobo_moniker_use_extender ("OAFIID:Bonobo_MonikerExtender_stream", moniker, options, requested_interface, ev);

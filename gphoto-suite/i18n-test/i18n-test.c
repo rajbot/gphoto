2cab4f77e4c9da914bfde9cf3e8df3574025e491
hun <hun@67ed7778-7388-44ab-90cf-0a291f65f57c>
2005-05-07 13:02:47 +0000

    make i18n example work
    git-svn-id: svn://svn.code.sf.net/p/gphoto/code/trunk@7964 67ed7778-7388-44ab-90cf-0a291f65f57c

--- gphoto-suite/i18n-test/i18n-test.c
+++ gphoto-suite/i18n-test/i18n-test.c
@@ -1,7 +1,17 @@
+#include "config.h"
+
 #include <stdio.h>
+#include <gettext.h>
+
+#ifdef ENABLE_NLS
+# define _(msgid) gettext(msgid)
+#else
+# define _(msgid) msgid
+#endif
 
 int main(int argc, char *argv[])
 {
 	printf("i18n-test\n");
 	printf("%s\n", _("Erk"));
+	return 0;
 }

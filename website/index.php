ae2877cdf88652c2e7e89bc99110cf10ddf972d4
marcusmeissner <marcusmeissner@67ed7778-7388-44ab-90cf-0a291f65f57c>
2014-01-10 21:54:40 +0000

    updated for 2.5.3
    git-svn-id: svn://svn.code.sf.net/p/gphoto/code/trunk@14695 67ed7778-7388-44ab-90cf-0a291f65f57c

--- website/index.php
+++ website/index.php
@@ -17,6 +17,14 @@
 
 
     	<p>
+	<b>libgphoto2 and gphoto2 2.5.3</b> have been
+	released. Read <a href="/news/"><b>what's new</b></a>.
+	Download it <a href="/download/"><b>now</b></a>.
+    	</p>
+	<div style="text-align: right;"><b>January 10th, 2014</b></div>
+    	<p>
+
+    	<p>
 	<b>libgphoto2 and gphoto2 2.5.2</b> have been
 	released. Read <a href="/news/"><b>what's new</b></a>.
 	Download it <a href="/download/"><b>now</b></a>.
@@ -85,7 +93,7 @@
     ready to use set of digital camera software
     applications for Unix-like systems, written by a whole team of dedicated
     volunteers around the world. It supports more than 
-    <a href="/proj/libgphoto2/support.php">1700 cameras</a></p>
+    <a href="/proj/libgphoto2/support.php">1800 cameras</a></p>
     <p>gPhoto2 runs on a large range of UNIX-like operating system, including
     Linux, FreeBSD, NetBSD, MacOS X, etc. gPhoto is provided by major Linux distributions
     like Debian GNU/Linux, Ubuntu, Gentoo, Fedora, openSUSE, Mandriva, etc.</p>
